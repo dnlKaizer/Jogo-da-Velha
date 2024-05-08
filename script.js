1cc0155edfb9d815ac78a1809124bf05cb2d3f11
dnlKaizer <danilokaizer@gmail.com>
2024-05-08 15:36:30 -0300

    Atualização na classe JogoDaVelha
    Os seguintes elementos foram adicionados:
        nJogadas    ->  Atributo que indica quantas jogadas já foram realizadas
        jogar(i, j) ->  Método que realiza uma jogada no index [i][j] da matriz, cujo símbolo é identificado automaticamente

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 class JogoDaVelha {
     matriz = [[-1,-1,-1],[-1,-1,-1],[-1,-1,-1]];
+    nJogadas = 0
 
     getMatriz() {
         const matrizAux = this.matriz.map((x) => x) // Copia o atributo matriz
         return matrizAux
+    }
+
+    jogar(i, j) {
+        // 0 indica que é O
+        // 1 indica que é X
+        if (this.matriz[i][j] == -1) {
+            this.nJogadas++;
+            this.matriz[i][j] = this.nJogadas % 2
+        }
     }
 }
 
