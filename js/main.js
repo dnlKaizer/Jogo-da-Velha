052cb9dc723bf282530108a4c2346a6f23a7b77d
dnlKaizer <danilokaizer@gmail.com>
2024-05-22 13:36:47 -0300

    Método restart()

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 import { Interface } from './Interface.js'
 import { JogoDaVelha } from './JogoDaVelha.js'
 
-window.clicar = function (index) {
+window.clicar = (index) => {
     jogo.jogarIndex(index)
     tela.atualizar()
+}
+
+window.restart = () => {
+    tela.restart()
+    jogo = new JogoDaVelha()
+    tela = new Interface(jogo)
 }
 
 let jogo = new JogoDaVelha()
