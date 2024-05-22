51c6d057811be172e1e015f70925b48c2bf43814
dnlKaizer <danilokaizer@gmail.com>
2024-05-22 14:02:57 -0300

    Mudança na estrutura do símbolo X
    Agora, ao invés de usar os pseudo-elementos before e after, foram utilizados divs

--- js/Interface.js
+++ js/Interface.js
@@ -11,9 +11,17 @@ export class Interface {
             if (mat.getIndiceByIndex(i) != -1) {
                 let div = cell.querySelector('div')
                 if (div == null) {
-                    let symbol = document.createElement('div')
                     let nJogadas = this.jogo.getNJogadas
+                    let symbol = document.createElement('div')
                     symbol.classList = `symbol${nJogadas % 2}`
+                    if (nJogadas % 2 == 1) {
+                        let before = document.createElement('div')
+                        let after = document.createElement('div')
+                        before.classList = 'symbol11'
+                        after.classList = 'symbol12'
+                        symbol.appendChild(before)
+                        symbol.appendChild(after)
+                    }
                     cell.appendChild(symbol)
                 }
             }
