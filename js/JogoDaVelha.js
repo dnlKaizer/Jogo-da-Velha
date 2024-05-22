a6a9a04bbecd5b71776c401812f94d7b36404951
dnlKaizer <danilokaizer@gmail.com>
2024-05-22 16:19:14 -0300

    Implementado código que salva o vencedor quando o jogo acaba

--- js/JogoDaVelha.js
+++ js/JogoDaVelha.js
@@ -1,10 +1,12 @@
 import { Matriz } from "./Matriz.js";
+import { Vencedor } from "./Vencedor.js";
 
 export class JogoDaVelha {
     constructor() {
         this.matriz = new Matriz()
         this.nJogadas = 0
         this.fim = false
+        this.vencedor
     }
 
     get getMatriz() {
@@ -19,19 +21,44 @@ export class JogoDaVelha {
         return this.fim
     }
 
-    jogar(i, j) {
-        if (this.fim) {
-            return
-        }
-        this.nJogadas++
-        this.matriz.alterar((this.nJogadas % 2), i, j)
-    }
-
     jogarIndex(index) {
         if (this.fim) {
             return
         }
         this.nJogadas++
         this.matriz.alterarByIndex((this.nJogadas % 2), index)
+        this.verificarVencedor()
+    }
+
+    verificarVencedor() {
+        for (let m = 0; m < 3; m++) {
+            if (this.valoresVetorIguais(this.matriz.getRow(m))) {
+                this.vencedor = new Vencedor(this.matriz.getRow(m)[0], 0, m)
+                this.fim = true
+            }
+            if (this.valoresVetorIguais(this.matriz.getColumn(m))) {
+                this.vencedor = new Vencedor(this.matriz.getColumn(m)[0], 1, m)
+                this.fim = true
+            }
+        }
+        if (this.valoresVetorIguais(this.matriz.getDiagonalPrincipal())) {
+            this.vencedor = new Vencedor(this.matriz.getIndice(1,1), 2, 0)
+            this.fim = true
+        }
+        if (this.valoresVetorIguais(this.matriz.getDiagonalSecundaria())) {
+            this.vencedor = new Vencedor(this.matriz.getIndice(1,1), 2, 1)
+            this.fim = true
+        }
+    }
+
+    valoresVetorIguais(array) {
+        for (let m = 0; m < 3; m++) {
+            for (let n = m + 1; n < 3; n++) {
+                if (array[m] == -1 || array[m] != array[n]) {
+                    return false
+                }
+            }
+        }
+        return true
     }
 }
