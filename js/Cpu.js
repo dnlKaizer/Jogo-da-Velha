b39513daa460c2a53191fdea76878a6add0a5a5f
dnlKaizer <danilokaizer@gmail.com>
2024-05-27 17:35:23 -0300

    Novas adições na classe Cpu
    Atributos:
        matriz
    
    Métodos:
        lerJogadasPossiveis()
        random(min, max)

--- js/Cpu.js
+++ js/Cpu.js
@@ -1,6 +1,21 @@
 export class Cpu {
     constructor(jogo, dificuldade) {
         this.jogo = jogo
+        this.matriz = jogo.getMatriz
         this.dificuldade = dificuldade
+    }
+
+    lerJogadasPossiveis() {
+        let jogadasPossiveis = []
+        for (let i = 0; i < 9; i++) {
+            if (this.matriz.getIndiceByIndex(i) == -1) {
+                jogadasPossiveis.push(i)
+            }
+        }
+        return jogadasPossiveis
+    }
+
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
 }
