b397b2b9eb7a1eb26b980b612279924fed2c4153
dnlKaizer <danilokaizer@gmail.com>
2024-05-29 15:38:19 -0300

    Criação da classe Jogada
    Atributos:
        index
        j
        i
        symbol
    
    Métodos:
        get getIndex()
        get getSymbol()
        isCentro()
        isCanto()
        isMeio()

---
+++ js/Jogada.js
@@ -0,0 +1,46 @@
+export class Jogada {
+
+    /** 
+     * @param {number} index
+     * @param {number} symbol
+    */
+    constructor(index, symbol) {
+        this.index = index
+        this.j = index % 3
+        this.i = (index - (index % 3)) / 3
+        this.symbol = symbol 
+    }
+
+    get getIndex() {
+        return this.index
+    }
+
+    get getSymbol() {
+        return this.symbol
+    }
+
+    isCentro() {
+        if (this.index == 4) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    isCanto() {
+        if (!this.isCentro()) {
+            if (this.index % 2 == 0) {
+                return true
+            }
+        }
+        return false
+    }
+
+    isMeio() {
+        if (this.index % 2 == 1) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
