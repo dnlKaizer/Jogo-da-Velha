ab845bb88037e8d2bda1d76559bf4961443c446d
dnlKaizer <danilokaizer@gmail.com>
2024-06-03 18:44:54 -0300

    Criação das funções
        clicar(indexCell)
        escolherModo(tipoModo)
        reiniciar()
        clicarSeletor()

---
+++ js/main.js
@@ -0,0 +1,28 @@
+import { Interface } from "./Interface.js";
+import { JogoDaVelha } from "./JogoDaVelha.js";
+
+let jogo = new JogoDaVelha()
+let tela = new Interface()
+
+/**
+ * 
+ * @param {number} indexCell 
+ */
+window.clicar = (indexCell) => {
+    
+} 
+
+/**
+ * @param {number} tipoModo 
+ */  
+window.escolherModo = (tipoModo) => {
+
+} 
+
+window.reiniciar = () => {
+
+} 
+
+window.clicarSeletor = () => {
+
+} 
