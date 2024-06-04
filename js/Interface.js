bbbeae2b2d7c34c7b0831efbf65e72c65e73ed0e
dnlKaizer <danilokaizer@gmail.com>
2024-06-04 13:18:58 -0300

    Métodos criarX() e criarO()

--- js/Interface.js
+++ js/Interface.js
@@ -25,4 +25,21 @@ export class Interface {
     set setJogo(jogo) {
         this.jogo = jogo
     }
+
+    criarX() {
+        const symbol1 = document.createElement('div')
+        const symbol11 = document.createElement('div')
+        const symbol12 = document.createElement('div')
+        symbol1.classList = 'symbol1'
+        symbol11.classList = 'symbol11'
+        symbol12.classList = 'symbol12'
+        symbol1.appendChild(symbol11)
+        symbol1.appendChild(symbol12)
+        return symbol1
+    }
+    criarO() {
+        const symbol0 = document.createElement('div')
+        symbol0.classList = 'symbol0'
+        return symbol0
+    }
 }
